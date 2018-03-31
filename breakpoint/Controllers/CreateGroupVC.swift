390b7db2e610da05ebd9354dfc0ff23c2cc3ebe3
Huy Trinh <dinhhuyams@gmail.com>
2018-03-31 06:43:09 +0300

    search for email account and add to group

---
+++ breakpoint/Controllers/CreateGroupVC.swift
@@ -0,0 +1,72 @@
+//
+//  CreateGroupVC.swift
+//  breakpoint
+//
+//  Created by iosdev on 30.3.2018.
+//  Copyright © 2018 HuyTrinh. All rights reserved.
+//
+
+import UIKit
+
+class CreateGroupVC: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var emailSearchTextField: UITextField!
+    @IBOutlet weak var groupMemberLbl: UILabel!
+    
+    var emailArray = [String]()
+    
+    @IBOutlet weak var doneBtn: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        emailSearchTextField.delegate = self
+        emailSearchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    @objc func textFieldDidChange() {
+        if emailSearchTextField.text == "" {
+            emailArray = []
+            tableView.reloadData()
+        } else {
+            DataService.instance.getEmail(forSearchQuery: emailSearchTextField.text!, handler: { (returnedEmailArray) in
+                self.emailArray = returnedEmailArray
+                self.tableView.reloadData()
+            })
+        }
+    }
+    
+    @IBAction func doneBtnWasPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func closeBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension CreateGroupVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emailArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as? UserCell else {
+            return UITableViewCell()
+        }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        cell.configureCell(profileImage: profileImage!, email: emailArray[indexPath.row], isSelected: true)
+        return cell
+    }
+}
+
+extension CreateGroupVC: UITextFieldDelegate {
+    
+}
---
+++ breakpoint/View/UserCell.swift
@@ -0,0 +1,33 @@
+//
+//  UserCell.swift
+//  breakpoint
+//
+//  Created by iosdev on 31.3.2018.
+//  Copyright © 2018 HuyTrinh. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var checkImage: UIImageView!
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureCell(profileImage: UIImage, email: String, isSelected: Bool) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        if isSelected {
+            self.checkImage.isHidden = false
+        } else {
+            self.checkImage.isHidden = true
+        }
+    }
+
+}
